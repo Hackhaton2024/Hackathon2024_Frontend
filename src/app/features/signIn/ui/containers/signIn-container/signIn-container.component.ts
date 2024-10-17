07fd064281be40bd94976f5d10ea2fe6fae02fe2
DESSERE <quentin.dessere@outlook.fr>
2024-10-17 14:09:24 +0200

    buton and route signup and signin created

---
+++ src/app/features/signIn/ui/containers/signIn-container/signIn-container.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signin',
+  template: '<h1>Signin Page</h1>',
+  styles: []
+})
+export class SigninComponent {}
---
+++ src/app/features/signUp/ui/containers/signUp-container/signUp-container.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  template: '<h1>Signup Page</h1>',
+  styles: []
+})
+export class SignupComponent {}
