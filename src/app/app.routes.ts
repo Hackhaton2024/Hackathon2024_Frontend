3dcb3ba8082f7238ceec690ebe39ab18cc522348
DESSERE <quentin.dessere@outlook.fr>
2024-10-17 13:09:47 +0200

    favorites and sign component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,27 @@
 import { Routes } from '@angular/router';
 import { HomeContainerComponent } from './features/home/ui/containers/home-container/home-container.component';
+import {FavoritesContainerComponent } from './features/design/ui/containers/favorites-container/favorites-container.component'; 
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeContainerComponent
-    }
+    },
+    // {
+    //     path: 'signUp',
+    //      component: signInContainerComponent
+    // },
+    // {
+    //     path: 'signIn',
+    //      component: signUpContainerComponent
+    // },
+    {
+        path: 'search',
+        component: FavoritesContainerComponent
+    },
+    {
+        path: 'favoris',
+        component: FavoritesContainerComponent
+    },
+   
 ];
--- src/app/features/home/ui/containers/home-container/home-container.component.ts
+++ src/app/features/home/ui/containers/home-container/home-container.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { DesignContainerComponent } from "../../../../design/ui/containers/design-container/design-container.component";
 import {LoginComponent} from '../../../../../features/design/ui/containers/loginContainer/login.component';
+import { FavoritesContainerComponent } from '../../../../design/ui/containers/favorites-container/favorites-container.component';
+import {AuthChoiceComponent} from '../../../../../features/design/ui/containers/sign-container/sign-container.component'
 
 @Component({
   selector: 'app-home-container',
   standalone: true,
-  imports: [DesignContainerComponent, LoginComponent],
+  imports: [DesignContainerComponent,LoginComponent, FavoritesContainerComponent, AuthChoiceComponent],
   templateUrl: './home-container.component.html',
   styleUrl: './home-container.component.scss'
 })
